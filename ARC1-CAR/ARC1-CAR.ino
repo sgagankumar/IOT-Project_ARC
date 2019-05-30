cf0cfa4cb7c18dc7fc7a147d6cac81194a8a9aba
Gagan Kumar <31625778+sgagankumar@users.noreply.github.com>
2019-05-30 20:37:14 +0530

    Add files via upload

---
+++ ARC1-CAR/ARC1-CAR.ino
@@ -0,0 +1,41 @@
+int ip = D1;
+int red_led = D2;
+int green_led = D3;
+int IR3= D4;
+int val =0;
+
+void setup() 
+{
+  pinMode(ip,INPUT);
+  pinMode(IR3,INPUT);
+  Serial.begin(9600);
+  pinMode(red_led,OUTPUT);
+  pinMode(green_led,OUTPUT);
+}
+
+void loop() 
+{
+  
+  val=digitalRead(ip);
+  val=1;
+  if(val == 1)
+  {
+      Serial.println("val high");
+      while(digitalRead(IR3)==1) //on red led while there is a car stuck
+      {   Serial.println("ir3 high :car detected");
+          digitalWrite(green_led,LOW);
+          digitalWrite(red_led,HIGH);
+          
+      }
+     
+         
+  }
+
+  digitalWrite(green_led,HIGH);
+  digitalWrite(red_led,LOW);
+      
+
+  delay(500);
+  // put your main code here, to run repeatedly:
+
+}
---
+++ ARC3-LCD/ARC3-LCD.ino
@@ -0,0 +1,77 @@
+#include <ESP8266WiFi.h>
+#include <LiquidCrystal_I2C.h>
+#include <Wire.h>
+LiquidCrystal_I2C lcd(0x3F, 16, 2);
+int ir1 = D1;
+int ir2=D2;
+
+void setup() {
+  // put your setup code here, to run once:
+
+   Wire.begin(D6,D7);   
+  
+   lcd.init();
+   
+   lcd.begin(16,2);
+   lcd.backlight();
+    
+    pinMode(ir1, INPUT);
+       pinMode(ir2, INPUT);
+    Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  lcd.clear();
+int ReadIr1 = digitalRead(ir1);
+int ReadIr2 = digitalRead(ir2);
+
+if(ReadIr1==LOW && ReadIr2==LOW)
+{ lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("THANK YOU");
+  lcd.setCursor(0,1);
+   lcd.print("TEQUED LABS");
+    delay(5000);
+    
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("DRIVE SAFE");
+  lcd.setCursor(0,1);
+   lcd.print("HAVE A GOOD DAY");
+    delay(5000);
+    
+  
+}
+else if(ReadIr1==LOW && ReadIr2==HIGH)
+{lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("TRAIN IS COMING");
+  lcd.setCursor(0,1);
+  lcd.print("PLEASE STOP");
+ 
+  delay(500);
+  
+
+  
+}
+  else if(ReadIr1==HIGH && ReadIr2==LOW)
+{lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("GATE CLOSING");
+  lcd.setCursor(0,1);
+  lcd.print("PLEASE WAIT");
+    delay(500);
+  
+      
+}
+
+  else if(ReadIr1==HIGH && ReadIr2==HIGH)
+{ lcd.clear();
+ lcd.setCursor(0,0);
+   lcd.print("TRAIN LEAVING");
+   lcd.setCursor(0,1);
+  lcd.print("PLEASE WAIT");
+      delay(500);
+}
+}
